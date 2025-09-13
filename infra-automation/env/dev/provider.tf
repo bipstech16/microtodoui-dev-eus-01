defdd16931252cc6e4d33ce8990693b4e32fb872
bipstech16 <168907429+bipstech16@users.noreply.github.com>
2025-09-13 14:56:30 +0530

    Update provider.tf

--- infra-automation/env/dev/provider.tf
+++ infra-automation/env/dev/provider.tf
@@ -8,7 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
-  use_cli = false
+  features {
+resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
+
   subscription_id = "c05900c4-9a99-417d-878e-413b907a6035"
 }
